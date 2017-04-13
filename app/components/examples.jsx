aa5a87a57ecb6b3a916c406642d00b4eda222764
TarekH86 <tarekalhindi.86@gmail.com>
2017-04-13 17:30:23 +0200

    Rename example page

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -6,7 +6,7 @@ class Examples extends Component {
     render() {
         return (
             <div>
-            <h3>Examples Component !</h3>
+            <h3>Examples Component</h3>
             <p>Welcome to examples Component</p>
             </div>
         );
